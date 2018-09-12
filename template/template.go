ce1d083a6500196ee11e82651573f71bf19bcb9b
ldc_ <934831065@qq.com>
2018-09-12 17:11:06 +0800

    add mini template

--- template/template.go
+++ template/template.go
@@ -11,6 +11,7 @@ import (
 const (
 	templateSendURL          = "https://api.weixin.qq.com/cgi-bin/message/template/send"
 	templateSubscribeSendURL = "https://api.weixin.qq.com/cgi-bin/message/template/subscribe"
+	templateMiniSendURL      = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?"
 )
 
 //Template 模板消息
@@ -27,16 +28,24 @@ func NewTemplate(context *context.Context) *Template {
 
 //Message 发送的模板消息内容
 type Message struct {
-	ToUser     string               `json:"touser"`          // 必须, 接受者OpenID
-	TemplateID string               `json:"template_id"`     // 必须, 模版ID
-	URL        string               `json:"url,omitempty"`   // 可选, 用户点击后跳转的URL, 该URL必须处于开发者在公众平台网站中设置的域中
-	Color      string               `json:"color,omitempty"` // 可选, 整个消息的颜色, 可以不设置
-	Data       map[string]*DataItem `json:"data"`            // 必须, 模板数据
-
+	ToUser      string               `json:"touser"`          // 必须, 接受者OpenID
+	TemplateID  string               `json:"template_id"`     // 必须, 模版ID
+	URL         string               `json:"url,omitempty"`   // 可选, 用户点击后跳转的URL, 该URL必须处于开发者在公众平台网站中设置的域中
+	Color       string               `json:"color,omitempty"` // 可选, 整个消息的颜色, 可以不设置
+	Data        map[string]*DataItem `json:"data"`            // 必须, 模板数据
 	MiniProgram struct {
 		AppID    string `json:"appid"`    //所需跳转到的小程序appid（该小程序appid必须与发模板消息的公众号是绑定关联关系）
 		PagePath string `json:"pagepath"` //所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar）
 	} `json:"miniprogram"` //可选,跳转至小程序地址
+}
+
+type MiniMessage struct {
+	ToUser          string               `json:"touser"`      // 必须, 接受者OpenID
+	TemplateID      string               `json:"template_id"` // 必须, 模版ID
+	Data            map[string]*DataItem `json:"data"`        // 必须, 模板数据
+	Page            string               `json:"page"`
+	FormID          string               `json:"form_id"`
+	EmphasisKeyword string               `json:"emphasis_keyword"`
 }
 
 //DataItem 模版内某个 .DATA 的值
@@ -49,6 +58,10 @@ type resTemplateSend struct {
 	util.CommonError
 
 	MsgID int64 `json:"msgid"`
+}
+type resTemplateMiniSend struct {
+	util.CommonError
+	TemplateID string `json:"template_id"`
 }
 
 //Send 发送模板消息
@@ -71,6 +84,28 @@ func (tpl *Template) Send(msg *Message) (msgID int64, err error) {
 		return
 	}
 	msgID = result.MsgID
+	return
+}
+
+func (tpl *Template) MiniSend(msg *MiniMessage) (templateID string, err error) {
+	var accessToken string
+	accessToken, err = tpl.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s?access_token=%s", templateMiniSendURL, accessToken)
+	response, err := util.PostJSON(uri, msg)
+
+	var result resTemplateMiniSend
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("template msg send error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+	templateID = result.TemplateID
 	return
 }
 
