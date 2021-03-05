e9ee9eff7556a6d52e1aee410a60082f9e73546b
dcsunny <934831065@qq.com>
2021-03-05 18:22:37 +0800

    小程序新增url scheme接口

---
+++ miniprogram/urlscheme.go
@@ -0,0 +1,53 @@
+package miniprogram
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dcsunny/wechat/define"
+
+	"github.com/dcsunny/wechat/common_error"
+	"github.com/dcsunny/wechat/util"
+)
+
+const (
+	urlschemeGenerateUrl = "https://api.weixin.qq.com/wxa/generatescheme"
+)
+
+type UrlschemeGenerateReq struct {
+	JumpWxa    UrlschemeGenerateJumWxa `json:"jumpWxa"`
+	IsExpire   bool                    `json:"is_expire"`
+	ExpireTime int64                   `json:"expire_time"`
+}
+
+type UrlschemeGenerateJumWxa struct {
+	Path  string `json:"path"`
+	Query string `json:"query"`
+}
+
+type UrlschemeGenerateResp struct {
+	define.CommonError
+	Openlink string `json:"openlink"`
+}
+
+func (wxa *MiniProgram) UrlschemeGenerate(req UrlschemeGenerateReq) (UrlschemeGenerateResp, error) {
+	accessToken, err := wxa.GetAccessToken()
+	if err != nil {
+		return UrlschemeGenerateResp{}, err
+	}
+
+	uri := fmt.Sprintf("%s?access_token=%s", urlschemeGenerateUrl, accessToken)
+	response, err := util.PostJSON(uri, req)
+	if err != nil {
+		return UrlschemeGenerateResp{}, err
+	}
+	var result UrlschemeGenerateResp
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return UrlschemeGenerateResp{}, err
+	}
+	if result.ErrCode != 0 {
+		return UrlschemeGenerateResp{}, common_error.CommonErrorHandle(result.CommonError, wxa.Context, "urlschemeGenerate")
+	}
+	return result, nil
+}
